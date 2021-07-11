3549e80b7b9e9a21fbfdb1fe9a11fec33a4a7047
Administrator <Administrator@NAMAKSHENAS>
2021-07-11 15:16:16 +0430

    domain and infra

---
+++ CleanArch/CleanArch.Infra.Data/Contex/UniversityDBContext.cs
@@ -0,0 +1,20 @@
+﻿using CleanArch.
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Contex
+{
+    public class UniversityDBContext : DbContext
+    {
+        public  UniversityDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+    }
+}
